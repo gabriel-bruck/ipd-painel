28ea15ab417d2f05ecf8ea3eba0b0dab657b387d
Gabriel Bruck <gabriel.bruck@quaest.com.br>
2026-08-03 11:12:32 -0300

    gitignore

--- projeto/models.py
+++ projeto/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 
 class Projeto(models.Model):
     nome = models.CharField(max_length=100)
