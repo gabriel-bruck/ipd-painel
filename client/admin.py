e36015baad8c6eba508a03f830978c5c95cfe281
Gabriel Bruck <gabriel.bruck@quaest.com.br>
2026-09-04 10:34:54 -0300

    add tipo ipd

--- client/admin.py
+++ client/admin.py
@@ -7,11 +7,14 @@ from .models import ProjetoIPD, ProjetoCliente
 
 @admin.register(ProjetoCliente)
 class ProjetoClienteAdmin(admin.ModelAdmin):
-    list_display = ('id','nome', 'cliente', 'descricao', 'tipo_ipd')
+    list_display = ('id', 'nome', 'cliente', 'descricao', 'get_tipo_ipd')
     prepopulated_fields = {'slug': ('nome',)}
     search_fields = ('nome', 'cliente')
     filter_horizontal = ('projetos_ipd',)
 
+    @admin.display(description='Tipo IPD')
+    def get_tipo_ipd(self, obj):
+        return obj.get_tipo_ipd_display()
 
 @admin.register(ProjetoIPD)
 class ProjetoIPDAdmin(admin.ModelAdmin):
